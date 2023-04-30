b35eac2a9e2fe217bed93013d487fd3cab834c54
grisha <test@gmail.com>
2023-04-30 19:19:46 +0300

    динамічний сайдбар з права, і логотип які міняються в адмінці

--- wp-content/themes/cocojambo/functions.php
+++ wp-content/themes/cocojambo/functions.php
@@ -1,9 +1,9 @@
 <?php
 require_once __DIR__ . '/class/Cocojambo_Walker.php';
 add_action('wp_enqueue_scripts', 'cocojambo_require_scripts');
-add_action('after_setup_theme', 'add_post_thumbnails_setup');
-
-add_filter( 'navigation_markup_template', 'navigation_markup_template_filter', 10, 2 );
+add_action('after_setup_theme', 'cocojambo_add_post_thumbnails_setup');
+add_action('widgets_init', 'cocojambo_widgets_init');
+add_filter( 'navigation_markup_template', 'cocojambo_navigation_markup_template_filter', 10, 2 );
 function cocojambo_require_scripts() {
 	wp_enqueue_style('cocojambo-style-bootstrap',
 		get_template_directory_uri() . '/assets/css/bootstrap.min.css');
@@ -12,8 +12,12 @@ function cocojambo_require_scripts() {
 	wp_enqueue_script('cocojambo-script-bootstrap',
 		get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js');
 }
-function add_post_thumbnails_setup() {
+function cocojambo_add_post_thumbnails_setup() {
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'custom-logo', [
+		'width' =>'40',
+		'height' =>'40'
+	]);
 	register_nav_menus([
 		'header_menu' => 'Верхнє меню',
 		'right_menu' => 'Меню з права',
@@ -21,10 +25,18 @@ function add_post_thumbnails_setup() {
 	]);
 }
 
-function navigation_markup_template_filter( $template, $css_class ): string {
+function cocojambo_navigation_markup_template_filter( $template, $css_class ): string {
 
 	return '
 	<nav class="navigation" aria-label="%4$s">
 		<div class="nav-links">%3$s</div>
 	</nav>';
+}
+
+function cocojambo_widgets_init() {
+	register_sidebar([
+		'name' =>'Сайдбар з права',
+		'id' =>'right_sidebar',
+		'description' =>'Віджети з права на головні сторінці'
+	]);
 }
