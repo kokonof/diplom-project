f1a503d4fc6f2b4ebb77aaa455ee26b945f3727c
grisha <test@gmail.com>
2023-05-10 22:08:50 +0300

    тестовий клас для роботи з тайтлом

---
+++ wp-content/plugins/cocojambo/src/Study.php
@@ -0,0 +1,30 @@
+<?php
+
+class CocojamboStudy {
+
+	public function convertTitle() {
+		add_filter('the_title', function ($title, $post_id) {
+			return mb_convert_case($title, MB_CASE_TITLE);
+		}, 10, 2);
+	}
+
+	public function addPrefixToPostTitle() {
+		add_filter('the_title', function ($title, $post_id = null) {
+			if (!is_admin() && 'post' === get_post_type($post_id)) {
+				$prefix = 'Префікс: '; // вкажіть свій префікс
+				$title = $prefix . $title;
+			}
+			return $title;
+		}, 10, 2);
+
+	}
+
+
+
+
+
+
+}
+
+
+
