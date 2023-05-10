62cba991b115ee92e3953b8161840a612d8c06af
grisha <test@gmail.com>
2023-05-11 00:04:01 +0300

    створення таблиці в базі при активовувані плагіна

--- wp-content/plugins/cocojambo/cocojambo.php
+++ wp-content/plugins/cocojambo/cocojambo.php
@@ -8,9 +8,27 @@ Author: Волков Гріша
 Author URI:
 */
 
-if (!defined('ABSPATH')) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 require_once 'autoload.php';
+
+function activationPlugin() {
+	if ( PHP_MAJOR_VERSION < 7 ) {
+		die( ' Потрібно PHP >= 7' );
+	}
+
+	global $wpdb;
+	$query = "CREATE TABLE IF NOT EXISTS `{$wpdb->prefix}test`
+				(
+				    `id` INT NOT NULL AUTO_INCREMENT ,
+					 `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)
+    			) ENGINE = InnoDB;";
+	$wpdb->query( $query );
+}
+
+register_activation_hook( __FILE__, 'activationPlugin' );
 
 $cocojambo_study = new CocojamboStudy();
 $cocojambo_study->convertTitle();
--- wp-content/plugins/cocojambo/src/LeftMenu.php
+++ wp-content/plugins/cocojambo/src/LeftMenu.php
@@ -2,22 +2,21 @@
 
 class CocojamboLeftMenu {
 
-	public function __construct(  ) {
+	public function __construct() {
 		$this->addMenuItem();
 	}
 
-	public function addMenuItem(  ) {
-		add_action('admin_menu', [$this, 'add_prefix_menu_item']);
-
+	public function addMenuItem() {
+		add_action( 'admin_menu', [ $this, 'addPrefixMenuItem' ] );
 	}
 
-	public function add_prefix_menu_item() {
+	public function addPrefixMenuItem() {
 		add_menu_page(
 			'Мощний плагін', // назва, яка буде відображена в меню
 			'Мощний плагін', // назва, яка буде відображена в меню
 			'manage_options', // рівень доступу користувачів до пункту меню
 			'add-prefix-to-post-title', // унікальний ідентифікатор пункту меню
-			[$this,'add_prefix_to_post'] // функція, яка викликається при виборі пункту меню
+			[ $this, 'addPrefixToPost' ] // функція, яка викликається при виборі пункту меню
 		);
 		add_submenu_page(
 			'add-prefix-to-post-title',
@@ -25,11 +24,14 @@ class CocojamboLeftMenu {
 			'Підпункт для меню', // назва, яка буде відображена в меню
 			'manage_options', // рівень доступу користувачів до пункту меню
 			'add_prefix_to_post', // унікальний ідентифікатор пункту меню
-			[$this,'add_prefix_to_post'] // функція, яка викликається при виборі пункту меню
+			[ $this, 'addPrefixToPost' ] // функція, яка викликається при виборі пункту меню
 		);
 	}
-	public function add_prefix_to_post() {
+
+	public function addPrefixToPost() {
 		echo 'Тут інформація про плагін якісь дашборди';
 	}
 
 }
+
+
