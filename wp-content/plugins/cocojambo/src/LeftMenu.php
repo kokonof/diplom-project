63de1831c2455be2bbd6ee858858fdef61b7c341
grisha <test@gmail.com>
2023-05-13 14:05:03 +0300

    пункт в налаштуваннях

--- wp-content/plugins/cocojambo/src/LeftMenu.php
+++ wp-content/plugins/cocojambo/src/LeftMenu.php
@@ -24,12 +24,23 @@ class CocojamboLeftMenu {
 			__('Settings','cocojambo'), // назва, яка буде відображена в меню
 			'manage_options', // рівень доступу користувачів до пункту меню
 			'add_prefix_to_post', // унікальний ідентифікатор пункту меню
-			[ $this, 'addPrefixToPost' ] // функція, яка викликається при виборі пункту меню
+			[ $this, 'addSettings' ] // функція, яка викликається при виборі пункту меню
 		);
 	}
 
 	public function addPrefixToPost() {
 		echo '<div class="wrap"> <h2>' . __('Create New Recommendation','cocojambo') . ' </h2></div>';
+	}
+
+	public function addSettings() {
+		echo '<div class="wrap">
+ 				<h1>' . _e('General Settings','cocojambo') . ' </h1>' .
+		    settings_errors()
+			. '<form action="options.php" method="post"> '.
+			settings_fields('cocojambo_main_group').
+		     do_settings_sections('add-prefix-to-post-title').
+			 submit_button()
+			.'</form></div>';
 	}
 
 }
