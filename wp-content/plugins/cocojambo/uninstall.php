92be05207b6888dbc32a6f32d70a20cfd5d586e4
grisha <test@gmail.com>
2023-05-14 15:08:55 +0300

    видалення постів які робляться через плігін

--- wp-content/plugins/cocojambo/uninstall.php
+++ wp-content/plugins/cocojambo/uninstall.php
@@ -5,3 +5,12 @@ if (! defined('WP_UNINSTALL_PLUGIN')) {
 
 global $wpdb;
 $wpdb->query("DROP TABLE IF EXISTS `{$wpdb->prefix}test`");
+
+$wpdb->query( "DELETE p, pm
+  FROM `{$wpdb->prefix}posts` p
+ INNER
+  JOIN `{$wpdb->prefix}postmeta` pm
+    ON pm.post_id = p.ID
+ WHERE p.post_type = 'book'" );
+
+$wpdb->query( "DELETE FROM `{$wpdb->prefix}posts` WHERE post_type = 'book'" );
