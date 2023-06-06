8a1d7a16216c50f461a2a8e1a94ff0797d0d3dfb
grisha <test@gmail.com>
2023-06-06 22:21:53 +0300

    правка слайда

--- wp-content/plugins/cocojambo/assets/public/public.js
+++ wp-content/plugins/cocojambo/assets/public/public.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function ($) {
         el.slideBox({
             position: 'bottom right', // can be [bottom|middle|top] and [left|center|right]
             appearsFrom: 'right', // can be [left|top|right|bottom]
-            slideDuration: 500, // animation duration in ms
-            target: 'h2', // can be a string (jQuery selector) or an integer (offset in px)
+            slideDuration: 2000, // animation duration in ms
+            target: 'h4', // can be a string (jQuery selector) or an integer (offset in px)
             closeLink: '#close' // a string that is the jQuery selector of the closing element
         });
     }
--- wp-content/plugins/cocojambo/templates/public/slide-template.php
+++ wp-content/plugins/cocojambo/templates/public/slide-template.php
@@ -2,12 +2,13 @@
 	<h3>
 		<?php echo $slide['title'] ?>
 		<small style="float: right;">
-			<a href="#" id="close">close</a>
+			<a href="#" id="close">X</a>
 		</small>
 	</h3>
 	<div>
 		<?php echo $slide['content'] ?>
 	</div>
 
-    <button class="btn btn-outline-info"> Читати далі >>>></button>
+    <a href="http://wordpress/%d0%b7%d0%bf%d0%b8%d1%81-43-%d0%b7%d0%b0%d0%ba%d1%80%d1%96%d0%bf%d0%bb%d0%b5%d0%bd%d0%b8%d0%b9"
+       class="btn btn-outline-info"> Читати далі >>></a>
 </div>
